badd8a5854e087bbc2dbf9fd6654977fbb3aa8cb
anuuo <54521617+anuuo@users.noreply.github.com>
2019-09-11 16:37:52 +0800

    Add files via upload

---
+++ count.js
@@ -0,0 +1,10 @@
+var count = 0;
+
+function plus(){
+    count ++;
+    document.getElementById("count").value = count;
+}
+function minus(){
+    count --;
+    document.getElementById("count").value = count;
+}
